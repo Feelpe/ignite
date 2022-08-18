490f1e03549b15e73e56ca28194be69f283a8687
felipe <felipesscreator@gmail.com>
2022-08-18 15:09:39 -0300

    feat: repository data on components

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,14 +3,6 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
-//https://api.github.com/users/Feelpe/repos
-
-const repository = {
-  name: 'undefined',
-  description: 'Forms in React',
-  link: 'https://github.com/'
-}
-
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([]);
 
@@ -25,7 +17,9 @@ export function RepositoryList() {
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        })}
       </ul>
     </section>
   )
