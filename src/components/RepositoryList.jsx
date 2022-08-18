55b76b61f3debdeee6768ef00f4f6ccdc09bc1f3
felipe <felipesscreator@gmail.com>
2022-08-18 15:03:12 -0300

    feat: useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,14 +1,25 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
+//https://api.github.com/users/Feelpe/repos
+
 const repository = {
-  name: 'unform',
+  name: 'undefined',
   description: 'Forms in React',
   link: 'https://github.com/'
 }
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/Feelpe/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, [])
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
